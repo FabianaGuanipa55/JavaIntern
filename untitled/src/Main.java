0594c0d0bb370b8268914db67e9046599815babe
f.guanipa.melendez@accenture.com <f.guanipa.melendez@accenture.com>
2025-03-28 10:59:38 +0100

    Primo commit: aggiunta classe Main al progetto java

---
+++ untitled/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[]args){
+        System.out.println("Questa è prima riga");
+        System.out.println("Questa è la seconda riga");
+
+    }
+}
