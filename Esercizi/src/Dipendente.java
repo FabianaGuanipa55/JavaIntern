c9163ac3dfb54776cf45e3b51a799db8d2f2e598
FabianaGuanipa55 <f.guanipa.melendez@accenture.com>
2025-03-31 17:08:29 +0200

    Add files via upload

---
+++ Esercizi/src/Dipendente.java
@@ -0,0 +1,12 @@
+public class Dipendente {
+    String nome;
+    String cognome;
+    int salarioBase;
+    public Dipendente(){
+    }
+    public Dipendente(String nome, String cognome, int salarioBase) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.salarioBase = salarioBase;
+    }
+}
---
+++ Esercizi/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager("Anna", "Bianchi", 500, 50);
+        System.out.println("Salario totale con il e di: " + manager.calcolaSalario());
+    }
+}
+
