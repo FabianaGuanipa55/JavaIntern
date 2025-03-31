529dd9b7d645497f28fed164b8eb9bc711dfe264
f.guanipa.melendez@accenture.com <f.guanipa.melendez@accenture.com>
2025-03-31 09:55:44 +0200

    canc cartela untitled dal repository

--- untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main (String[]args){
-        System.out.println("Questa è la prima riga");
-        System.out.println("Questa è la seconda riga");
-
-    }
-}
