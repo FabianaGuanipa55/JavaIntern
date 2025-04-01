a88e5a52381534e3c6e09339b0bb86ff5fc37db2
f.guanipa.melendez@accenture.com <f.guanipa.melendez@accenture.com>
2025-04-01 12:49:10 +0200

    Rimosse

--- Esercizi/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Manager manager = new Manager("Anna", "Bianchi", 500, 50);
-        System.out.println("Salario totale con il e di: " + manager.calcolaSalario());
-    }
-}
-
--- Exercises/src/Mammifero.java
+++
@@ -1,2 +0,0 @@
-public interface Mammifero extends Animale {
-}
