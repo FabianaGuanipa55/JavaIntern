0bbb850f8d9b017bd24e3657b7851b02f4635b49
f.guanipa.melendez@accenture.com <f.guanipa.melendez@accenture.com>
2025-04-08 14:36:47 +0200

    ..

--- Exercises/src/matrici/ArrayBidimensionale.java
+++ Exercises/src/matrici/ArrayBidimensionale.java
@@ -5,48 +5,64 @@ public class ArrayBidimensionale {
         //1 Data una matrice quadrata (NxN) di numeri interi, scrivi un programma Java che calcoli separatamente:
         //	la somma degli elementi di ogni riga
         //	la somma degli elementi di ogni colonna
-    /*    int[][] matrice = {{1, 2},
-                            {3, 4}};
-        int x = matrice.length;
-        int sommaRiga = 0;
-        int sommaColonna=0;
-        for (int i = 0; i < matrice.length; i++) {
-            for (int j = 0; i < matrice.length; j++) {
+
+        int[][] matrice = {
+                {1, 1,1},
+                {3, 3,3}
+        };
+        int riga = matrice.length;
+        int colonna=matrice[0].length;
+        for (int i = 0; i < riga; i++) {
+            int sommaRiga = 0;
+            for (int j = 0; j < colonna; j++) {
                 sommaRiga += matrice[i][j];
 
             }
-            System.out.println(sommaRiga);
+            System.out.println(""+ sommaRiga);
         }
-        for(int j=0; j<matrice.length;j++){
 
-            for(int i=0;i<matrice.length;i++){
+        for(int j=0; j<colonna;j++){
+            int sommaColonna=0;
+            for(int i=0;i<riga;i++){
                 sommaColonna += matrice [i] [j];
 
             }
-            System.out.println(sommaColonna);
+            System.out.println("" + sommaColonna);
         }
 
-     */
+
+
         //2 Data una matrice di dimensioni MxN e un valore intero da cercare,
         // scrivi un programma Java che verifichi se il numero è presente nella matrice
         // e restituisca tutte le posizioni (righe e colonne) in cui appare.
-        int [] [] matrice= {{1,2,3,
-                            4,5,6,
-                            7,8,9}};
-        int valore =6;
-        int poszione [] [] =trovaValore(matrice, valore);//provided=[][]
-        System.out.println("Il valore " + valore + "è stato trovato alla posizione" + poszione);
+    /*     int[][] matrice = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 6, 6}
+        };
+        int valore = 6;
+        trovaValore(matrice, valore);
 
     }
-    public static int [] [] trovaValore(int [] [] matrice, int valore) {
+
+    public static void trovaValore(int[][] matrice, int valore) {
+        int conteggio= 0;
         for (int i = 0; i < matrice.length; i++) {
-            for (int j = 0; j < matrice.length; j++) {
+            for (int j = 0; j < matrice[0].length; j++) { //meglio da [i]
                 if (matrice[i][j] == valore) {
-                    System.out.println("" + i + j);
+                    conteggio++;
+                    System.out.println("Il valore " + valore + " è stato trovato alla posizione " + i + " " + j);
+
                 }
             }
         }
-        return;
+        if(conteggio<=0){
+            System.out.println("il valore non è stato trovato");
+        }
+        System.out.println("il numero delle occorrenze: " + conteggio);ù
+
+     */
+
     }
 }
 
